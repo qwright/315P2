905013c9fbd2758722b52cba6b6921e566d16fbf
quinn <qfwright@outlook.com>
2020-11-05 09:10:35 -0800

    producer and consumers now interact, updated readme

--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -20,17 +20,17 @@ public:
     inline void notify( int tid ) {
         std::unique_lock<std::mutex> lock(mtx);
         count++;
-        cout << "thread " << tid <<  " notify" << endl;
+        //cout << "thread " << tid <<  " notify" << endl;
         //notify the waiting thread
         cv.notify_one();
     }
     inline void wait( int tid ) {
         std::unique_lock<std::mutex> lock(mtx);
         while(count == 0) {
-            cout << "thread " << tid << " wait" << endl;
+            //cout << "thread " << tid << " wait" << endl;
             //wait on the mutex until notify is called
             cv.wait(lock);
-            cout << "thread " << tid << " run" << endl;
+            //cout << "thread " << tid << " run" << endl;
         }
         count--;
     }
@@ -58,16 +58,34 @@ int Request::getID(){
 	return request_ID;
 }
 
+//global variables (shared between threads)
+
+Semaphore S(3); //can we get this constructed more dynamically?
+queue<Request> request_queue; //request queue
+
 //sleep a thread for a random amount of time
 void sleep_master()
 {
 	sleep(rand() % 5 + 1); // sleep randomly for 1-5s
 }
 
+//slaves(i.e. consumers) enter here
 void *thread_routine(void *id)
 {
-	printf("Thread #%ld entered\n", (long)id);
-	pthread_exit(NULL);//kill - debug
+	//printf("Thread #%ld entered\n", (long)id);
+	while(true){
+		S.wait((long)id);
+		if(!request_queue.empty()){
+			Request top = request_queue.front();
+			request_queue.pop();
+			cout << "Consumer " << (long)id << ": assigned Req: " << top.getID() << " for " << top.length << "s" << endl;
+			sleep(top.length);
+			cout << "Consumer " << long(id) << ": completed Req: " << top.getID() << endl;
+		}
+		S.notify((long)id);
+	}	
+
+	//pthread_exit(NULL);//kill - debug
 }
 
 //main entry point
@@ -77,7 +95,6 @@ int main()
 	cout << "Input amount of Slave threads: ";
 	cin >> N;
 
-	queue<Request> request_queue; //request queue
 	int ID[N]; //thread ids
 	pthread_t threads[N]; //threads
 	
@@ -92,7 +109,7 @@ int main()
 	while(true){
 		Request next_request(count);
 		curr_time = time(0);
-		cout << "Producer: New request ID: " << next_request.getID() << ", L= " << next_request.length << "(" << ctime(&curr_time) << ")" << endl;
+		cout << "Producer: New request ID: " << next_request.getID() << ", L= " << next_request.length << "time:" << ctime(&curr_time) << endl;
 		request_queue.push(next_request);
 		//sleep
 		sleep_master();
