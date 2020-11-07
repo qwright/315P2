3d6e17da64b9624fe8bf44772fc1b58fd7d6b2d8
jesseplamondon <jesse.plamondon57@gmail.com>
2020-11-07 13:14:28 -0800

    Updated Request, generate request and sleep_master

--- CppJava/main.java
+++ CppJava/main.java
@@ -6,43 +6,43 @@ import java.util.concurrent.locks.Lock;
 
 public class main{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Scanner in = new Scanner(System.in);
 		int N;
-		MyQueue request_queue = new MyQueue(); //debug
+		MyQueue request_queue = new MyQueue();
 		System.out.println("Input amount of Slave threads: ");
 		N = in.nextInt();
 		int ID[] = new int[N];
 		Thread threads[] = new Thread[N];
 		
 		//generate slaves
-		for(int i = 0; i<N; i++) {
+		int request_id = 0;
+		for (int id : ID){
+			//TODO: Implement slave generating
 			//ID[i] = pthread_create(&threads[i], NULL, thread_routine, (void*)i);
+			
+			request_id++;
 		}
 		
 		int count = 0;
 		long curr_time = System.currentTimeMillis();
 		
 		//generate requests
-		while(true) { //not sure about true
-			if(request_queue.size() != 5) { //arbitrary thread size
-				Request next_request = new Request(count);
-				System.out.println("Producer: New request ID: " + next_request.getID() 
-					+ ", L= " /*+ next_request.length */+ "time: " + curr_time);
-				request_queue.push(next_request);
-				count++;
-			}
-			//sleep
-			//sleep_master()
-		}
-	}
+		//generate requests
+        while(true) { //not sure about true
+            if(request_queue.size() != 5) { //arbitrary thread size
+                int rlength = (int)(1+Math.random()*10); // random length between 1-10s
+                Request next_request = new Request(count, rlength);
+                System.out.println("Producer: New request ID: " + next_request.getID() 
+                    + ", L= "+next_request.getLength()+ "time: " + curr_time);
+                request_queue.push(next_request);
+                count++;
+            }
+        }
+     }
 	
-	void sleep_master() {
-		sleep((int)(1 + Math.random() * 5)); //sleep randomly for 1-5 seconds
-	}
-	
-	void sleep(int i){
-		
+	public static void sleep_master() throws InterruptedException {
+		Thread.sleep((int)(1 + Math.random() * 5)); //sleep randomly for 1-5 seconds
 	}
 	
 	void thread_routine(int id, MyQueue request_queue, Thread[] threads) {
@@ -53,11 +53,12 @@ public class main{
 				Request top = request_queue.front();
 				request_queue.dequeue();
 				System.out.println("Consumer " + id + ": assigned Req: " + top.getID() + " for " 
-						+ top.length + "s");
-				S.notify(id);
-				sleep(top.length);
+						+ top.getLength() + "s");
+				//S.notify(id);
+				Thread.sleep(top.getLength());
 				System.out.println("Consumer " + id + ": completed Req: " + top.getID());
 			}
+			//TODO: implement notify for monitor
 			else S.notify(id);
 		}
 		//pthread_exit(NULL); //kill
@@ -65,24 +66,28 @@ public class main{
 	
 
 }
-class Request {
+	class Request {
 
-	private int request_ID;
-	int length = (int)(1+Math.random()*10); // random length between 1-10s
-	
-	public Request() {
-		
-	}
+	    private int request_ID;
+	    private int length; 
 
-	public Request(int r) {
-		request_ID = r;
-	}
+	    public Request() {
 
-	public int getID() {
-		return request_ID;
+	    }
+
+	    public Request(int r, int l) {
+	        request_ID = r;
+	        length = l;
+	    }
+
+	    public int getID() {
+	        return request_ID;
+	    }
+	    public int getLength() {
+	        return length;
+	    }
+
 	}
-	
-}
 
 class MyQueue{
     private Request arr[];          // array to store queue elements
@@ -114,6 +119,12 @@ class MyQueue{
     public synchronized int size() {
     	return count;
     }
+	public Request front() {
+		return front;
+	}
+	public int rear() {
+		return rear;
+	}
     
-
+    
 }
